78f8ca2768796c0dc02f6bf1f39df0b7f4c1bc58
eirstore <eirstore@stud.ntnu.no>
2017-11-09 00:10:29 +0100

    Extract numbers from server response

--- motor_rig/waterlinked/api_test.py
+++ motor_rig/waterlinked/api_test.py
@@ -1,4 +1,8 @@
 import urllib2
+import re
+
+def get_num(x):
+    return float(''.join(ele for ele in x if ele.isdigit() or ele == '.'))
 
 url = 'http://37.139.8.112:8000/api/v1/'
 
@@ -10,5 +14,6 @@ response = urllib2.urlopen('http://37.139.8.112:8000/api/v1/position/acoustic/ra
 
 AcousticRawPosition = response.read()
 
-print("Accoustic Filtered Position: %s\n", AcousticFilteredPosition)
-print("Accoustic Raw Position: %s\n", AcousticRawPosition)
+print "Accoustic Filtered Position: ", AcousticFilteredPosition
+print "Accoustic Raw Position: ",  AcousticRawPosition
+
